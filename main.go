5134afa8172ecbb5816a2a18e8c7fd7465149542
Rohan Dhamapurkar <dhamapurkar54@gmail.com>
2023-12-08 10:54:31 +0530

    refactor: for multiple connections

--- main.go
+++ main.go
@@ -11,6 +11,15 @@ import (
 	"golang.org/x/term"
 )
 
+var authorizedKeysMap map[string]bool
+
+var sshServerConfig *ssh.ServerConfig = &ssh.ServerConfig{
+	// Remove to disable password auth.
+	PasswordCallback: handlePasswordLogin,
+	// Remove to disable public key auth.
+	PublicKeyCallback: handlePublicKeyLogin,
+}
+
 func main() {
 	// Public key authentication is done by comparing
 	// the public key of a received connection
@@ -20,7 +29,7 @@ func main() {
 	// 	log.Fatalf("Failed to load authorized_keys, err: %v", err)
 	// }
 
-	authorizedKeysMap := map[string]bool{}
+	// authorizedKeysMap := map[string]bool{}
 	// for len(authorizedKeysBytes) > 0 {
 	// 	pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
 	// 	if err != nil {
@@ -33,36 +42,16 @@ func main() {
 
 	// An SSH server is represented by a ServerConfig, which holds
 	// certificate details and handles authentication of ServerConns.
-	config := &ssh.ServerConfig{
-		// Remove to disable password auth.
-		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
-			// Should use constant-time compare (or better, salt+hash) in
-			// a production setting.
-			if c.User() == "testuser" && string(pass) == "tiger" {
-				return &ssh.Permissions{
-					// Record the public key used for authentication.
-					Extensions: map[string]string{
-						"pubkey-fp": "password fingerprint",
-					},
-				}, nil
-			}
-			return nil, fmt.Errorf("password rejected for %q", c.User())
-		},
+	sshServerConfig.AddHostKey(getSvrPrivateKey())
 
-		// Remove to disable public key auth.
-		PublicKeyCallback: func(c ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissions, error) {
-			if authorizedKeysMap[string(pubKey.Marshal())] {
-				return &ssh.Permissions{
-					// Record the public key used for authentication.
-					Extensions: map[string]string{
-						"pubkey-fp": ssh.FingerprintSHA256(pubKey),
-					},
-				}, nil
-			}
-			return nil, fmt.Errorf("unknown public key for %q", c.User())
-		},
-	}
+	log.Println("SSH server is listening for incoming connections.")
 
+	listenForNewConnections()
+
+	
+}
+
+func getSvrPrivateKey() ssh.Signer {
 	privateBytes, err := os.ReadFile("ssh_server_key")
 	if err != nil {
 		log.Fatal("Failed to load private key: ", err)
@@ -72,27 +61,64 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to parse private key: ", err)
 	}
-	config.AddHostKey(private)
 
+	return private
+}
+
+func handlePublicKeyLogin(c ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissions, error) {
+	if authorizedKeysMap[string(pubKey.Marshal())] {
+		return &ssh.Permissions{
+			// Record the public key used for authentication.
+			Extensions: map[string]string{
+				"pubkey-fp": ssh.FingerprintSHA256(pubKey),
+			},
+		}, nil
+	}
+	return nil, fmt.Errorf("unknown public key for %q", c.User())
+}
+
+func handlePasswordLogin(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+	// Should use constant-time compare (or better, salt+hash) in
+	// a production setting.
+	if c.User() == "testuser" && string(pass) == "tiger" {
+		return &ssh.Permissions{
+			// Record the public key used for authentication.
+			Extensions: map[string]string{
+				"pubkey-fp": c.User(),
+			},
+		}, nil
+	}
+	return nil, fmt.Errorf("password rejected for %q", c.User())
+}
+
+func listenForNewConnections() {
 	// Once a ServerConfig has been configured, connections can be
 	// accepted.
 	listener, err := net.Listen("tcp", "0.0.0.0:2022")
 	if err != nil {
 		log.Fatal("failed to listen for connection: ", err)
 	}
-	nConn, err := listener.Accept()
-	if err != nil {
-		log.Fatal("failed to accept incoming connection: ", err)
-	}
+	for {
+		nConn, err := listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept incoming connection: %q", err)
+			continue
+		}
 
-	// Before use, a handshake must be performed on the incoming
-	// net.Conn.
-	conn, chans, reqs, err := ssh.NewServerConn(nConn, config)
-	if err != nil {
-		log.Fatal("failed to handshake: ", err)
-	}
-	log.Printf("logged in with key %s", conn.Permissions.Extensions["pubkey-fp"])
+		// Before use, a handshake must be performed on the incoming
+		// net.Conn.
+		conn, chans, reqs, err := ssh.NewServerConn(nConn, sshServerConfig)
+		if err != nil {
+			log.Printf("failed to handshake: %q", err)
+			continue
+		}
+		log.Printf("logged in with key %s", conn.Permissions.Extensions["pubkey-fp"])
+		go handleSingleConn(conn, chans, reqs)
 
+	}
+}
+
+func handleSingleConn(conn *ssh.ServerConn, chans <-chan ssh.NewChannel, reqs <-chan *ssh.Request) {
 	var wg sync.WaitGroup
 	defer wg.Wait()
 
@@ -122,9 +148,21 @@ func main() {
 		// "pty-req" and "env".  Here we handle only the
 		// "shell" request.
 		wg.Add(1)
-		go func(in <-chan *ssh.Request) {
-			for req := range in {
-				req.Reply(req.Type == "shell", nil)
+		go func(inChan <-chan *ssh.Request) {
+			for req := range inChan {
+				if req.Type == "pty-req" {
+					termLen := req.Payload[3]
+					term := string(req.Payload[4 : termLen+4])
+					log.Printf("PTY requested: %s", term)
+					if req.WantReply {
+						req.Reply(true, nil)
+					}
+					return
+				}
+				if req.Type == "shell" {
+					req.Reply(true, nil)
+					return
+				}
 			}
 			wg.Done()
 		}(requests)
@@ -142,7 +180,7 @@ func main() {
 				if err != nil {
 					break
 				}
-				fmt.Println(line)
+				fmt.Println("Out:" + line)
 			}
 		}()
 	}
